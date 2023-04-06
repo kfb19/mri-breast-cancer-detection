322bd6b583011f153eb3d64df6ea363620ff69b0
Kate Belson <kfb206@exeter.ac.uk>
2023-04-06 16:17:54 +0100

    Adding saving data to results file

--- Backend/eval_metrics.py
+++ Backend/eval_metrics.py
@@ -36,19 +36,28 @@ class Evaluation():
 
     def save_to_file(self, file_name, false_p, false_n, true_p, true_n):
         """ INSERT DOCSTRING """
-        folder_name = "results"
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
+        file_path = 'E:\\data\\output'
+        folder_name = '\\results'
+        path = file_path + folder_name
+        if not os.path.exists(path):
+            os.makedirs(path)
+            
+        path = path + "\\" + file_name
 
-        if not os.path.exists(file_name):
-            with open(file_name, "w", encoding="utf-8") as file:
+        if not os.path.exists(path):
+            with open(path, "w", encoding="utf-8") as file:
                 file.write(f"Test set accuracy: {self.accuracy}")
                 file.write(f"{true_p} true positive classifications\n")
                 file.write(f"{false_p} false positive classifications\n")
                 file.write(f"{true_n} true negative classifications\n")
                 file.write(f"{false_n} false negative classifications\n")
-                file.write(f"Negative predictive value: {self.npv}")
-                file.write(f"Positive predictive value: {self.ppv}")
-                file.write(f"Sensitivity: {self.sensitivity}")
-                file.write(f"Specificity: {self.specificity}")
+                file.write(f"Negative predictive value: {self.npv}\n")
+                file.write(f"Positive predictive value: {self.ppv}\n")
+                file.write(f"Sensitivity: {self.sensitivity}\n")
+                file.write(f"Specificity: {self.specificity}\n")
             file.close()
+        else:
+            print("File already exists, need new file name.\n")
+
+
+new = Evaluation(1, 2, 3, 4, "test.txt")
