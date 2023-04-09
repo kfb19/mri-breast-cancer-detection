d39fbfada062a72b22c7f3a69122a37eb8d52aa2
Kate Belson <kfb206@exeter.ac.uk>
2023-04-09 16:49:05 +0100

    Commenting evaluation metrics file
    Adding docstrings and helpful comments

--- Backend/evaluation/eval_metrics.py
+++ Backend/evaluation/eval_metrics.py
@@ -1,10 +1,34 @@
-""" DOC STRING """
+""" This module calculates the evaluation metrics
+for a CNN's performance, and saves them to a file
+for analysis.
+"""
 
 import os
 
 
 class Evaluation():
-    """ DOCSTRING HERE """
+    """ This class evaluated a CNN based off a selection of
+    metrics.
+
+        Attributes:
+            accuracy: the ratio of correctly predicted
+            classifications (true positives and true negatives)
+            to the total number of classifications.
+            npv: the result of dividing the number of true negative
+            classifications by the number of true negative plus the
+            number of false negatives.
+            ppv: aka precision, calculated by dividing the number of
+            true positive classifications by the number of true positives
+            plus the number of false positives.
+            sensitivity: calculated by dividing the number of true positive
+            classifications divided by the total number of positive scans
+            (true positives and false negatives).
+            specificity: the result of dividing the number of true negative
+            classifications by the total number of negative scans (true
+            negatives and false positives)
+        """
+
+    # Set attributes to a not-possible value (-1).
     accuracy = -1
     npv = -1
     ppv = -1
@@ -12,7 +36,18 @@ class Evaluation():
     specificity = -1
 
     def __init__(self, false_p, false_n, true_p, true_n, filename, folder):
-        """ DOCSTRING HERE """
+        """ Initialises the object for evaluating a CNN.
+
+        Args:
+            false_p: the number of false positives.
+            false_n: the number of false negatives.
+            true_p: the number of true positives.
+            true_n: the number of true negatives.
+            file_name: the name of the file to save to.
+            folder: the name of the folder to save to.
+        """
+
+        # Sets required variables.
         false_positives = false_p
         false_negatives = false_n
         true_positives = true_p
@@ -20,6 +55,7 @@ class Evaluation():
         file_name = filename
         folder_name = folder
 
+        # Calls functions to generate metrics then save to file.
         self.generate_metrics(false_positives, false_negatives,
                               true_positives, true_negatives)
         self.save_to_file(file_name, folder_name, false_positives,
@@ -27,32 +63,59 @@ class Evaluation():
 
     def generate_metrics(self, false_positives, false_negatives,
                          true_positives, true_negatives):
-        """ INSERT DOCSTRING """
+        """ Generates evaluation metrics from the number
+        of false positives, true positives, false negatives
+        and true negatives, and save these to the object.
+
+        Args:
+            false_positives: the number of false positives.
+            false_negatives: the number of false negatives.
+            true_positives: the number of true positives.
+            true_negatives: the number of true negatives.
+        """
+
+        # Calulcate accuracy.
         self.accuracy = (true_positives + true_negatives) / \
             (true_positives + true_negatives + false_positives +
              false_negatives)
+        # Calculate negative predicted value.
         self.npv = true_negatives / (true_negatives + false_negatives)
+        # Caluclate positive predicted value.
         self.ppv = true_positives / (true_positives + false_positives)
+        # Calculate sensitivity.
         self.sensitivity = true_positives / (true_positives + false_negatives)
+        # Calculate specificity.
         self.specificity = true_negatives / (true_negatives + false_positives)
 
     def save_to_file(self, file_name, folder, false_p, false_n, true_p,
                      true_n):
-        """ INSERT DOCSTRING """
-        file_path = 'E:\\data\\output'
-        folder_name = '\\results'
-        path = file_path + folder_name
-        if not os.path.exists(path):
-            os.makedirs(path)
+        """ Saves the calculated metrics to a file.
 
-        path = path + "\\" + folder
-        if not os.path.exists(path):
-            os.makedirs(path)
+        Args:
+            file_name: the name of the file to save to.
+            folder: the name of the folder to save to.
+            false_p: the number of false positives.
+            false_n: the number of false negatives.
+            true_p: the number of true positives.
+            true_n: the number of true negatives.
+        """
 
-        path = path + "\\" + file_name
+        file_path = 'E:\\data\\output\\results'
 
-        if not os.path.exists(path):
-            with open(path, "w", encoding="utf-8") as file:
+        # Creating required file paths.
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        file_path = os.path.join(file_path, folder)
+
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        file_path = os.path.join(file_path, file_name)
+
+        # Writes metrics to file, providing it doesn't already exist.
+        if not os.path.exists(file_path):
+            with open(file_path, "w", encoding="utf-8") as file:
                 file.write(f"Test set accuracy: {self.accuracy}")
                 file.write(f"{true_p} true positive classifications\n")
                 file.write(f"{false_p} false positive classifications\n")
