1520c38ca60f483cc2017b81b6cbd099b141f868
Kate Belson <kfb206@exeter.ac.uk>
2023-04-27 22:01:24 +0100

    Adding in tests docstrings

--- Backend/django/breastwise/breastcancer_api/tests.py
+++ Backend/django/breastwise/breastcancer_api/tests.py
@@ -1,4 +1,4 @@
-""" DOCSTRING """
+""" This module tests the API framework. """
 
 from unittest.mock import patch
 from django.test import TestCase
@@ -7,33 +7,41 @@ from rest_framework.test import APIClient
 
 
 class TestFileView(TestCase):
-    """ DOCSTRING """
+    """ This class sets up the testing framework. """
 
     def setUp(self):
-        """ DOCSTRING """
+        """ Defines the API client. """
         self.client = APIClient()
 
     @patch('os.listdir')
     def test_post_request_with_valid_file(self, mock_listdir):
-        """ DOCSTRING """
-        # Arrange
+        """ Tests valid file request.
+
+        Args:
+            mock_listdir: a mock list of directories.
+        """
+        # Arrange.
         mock_listdir.return_value = ['pre', '1st_pass', '2nd_pass', '3rd_pass']
         file_path = 'path/to/valid/file.zip'
         with open(file_path, 'rb') as file:
             data = {'file': file}
             expected_response = {'result': 'expected response'}
 
-        # Act
+        # Act.
         response = self.client.post('/api/file/', data, format='multipart')
 
-        # Assert
+        # Assert.
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, expected_response)
 
     @patch('os.listdir')
     def test_post_request_with_invalid_file(self, mock_listdir):
-        """ DOCSTRING """
-        # Arrange
+        """ Tests invalid file request.
+
+        Args:
+            mock_listdir: a mock list of directories.
+        """
+        # Arrange.
         mock_listdir.return_value = []
         file_path = 'path/to/invalid/file.zip'
         with open(file_path, 'rb') as file:
@@ -41,9 +49,9 @@ class TestFileView(TestCase):
             expected_response = {
                 'detail': 'Invalid file name: no series.zip found'}
 
-        # Act
+        # Act.
         response = self.client.post('/api/file/', data, format='multipart')
 
-        # Assert
+        # Assert.
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, expected_response)
